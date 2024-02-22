77f874f5e2a36a0bb522056319fe74b30d8c5b3b
Ghasem <ghasem.elyasi@gmail.com>
2024-02-22 21:12:14 +0100

    delete an item from a list

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,33 +7,33 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Text("Hello, \(name)")
-        
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
-
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        VStack {
-            Button("Show Sheet") {
-                showingSheet.toggle()
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRow)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
             }
         }
-        .padding()
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@raoufel")            
-        }
+    }
+    
+    func removeRow(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
