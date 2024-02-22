5059205c5f1c2c0fadf39efd4908134b3bbe9c7c
Ghasem <ghasem.elyasi@gmail.com>
2024-02-22 20:25:20 +0100

    using class and Observable with State property

---
+++ README.md
@@ -0,0 +1,8 @@
+# iExpense
+
+This is an iOS app built based on [Day 36 – Milestone: Projects 7](https://www.hackingwithswift.com/100/swiftui/36)
+
+
+## Tools
+- Xcode: 15.2
+- iOS: 17
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable
+class User {
+    var firstName = "Bilbo" {
+        didSet {
+            print("Firstname changed to: '\(firstName)'")
+        }
+    }
+    
+    
+    var lastName = "Baggins" {
+        didSet {
+            print("Lastname changed to '\(lastName)'")
+        }
+    }
+}
 
 struct ContentView: View {
+    @State private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(user.firstName) \(user.lastName)")
+          
+            TextField("First Name", text: $user.firstName)
+            TextField("Last Name", text: $user.lastName)
         }
         .padding()
     }
